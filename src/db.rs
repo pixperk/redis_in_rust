7863f06e187b14e759a654049917437af0977678
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-04-07 22:02:42 +0530

    all resp commands

--- src/db.rs
+++ src/db.rs
@@ -1,24 +1,25 @@
 use std::collections::HashMap;
 
-
-pub struct Database{
-    store : HashMap<String, String>, //key : value
+pub struct Database {
+    store: HashMap<String, String>, //key : value
 }
 
 impl Database {
     pub fn new() -> Self {
-        Database { store: HashMap::new() }
+        Database {
+            store: HashMap::new(),
+        }
     }
 
-    pub fn set(&mut self, key : String, value : String){
+    pub fn set(&mut self, key: String, value: String) {
         self.store.insert(key, value);
     }
 
-    pub fn get(&self, key : &str) -> Option<&String> {
+    pub fn get(&self, key: &str) -> Option<&String> {
         self.store.get(key)
     }
 
-    pub fn delete(&mut self, keys : &[String]) -> usize {
+    pub fn delete(&mut self, keys: &[String]) -> usize {
         let mut removed = 0;
         for key in keys {
             if self.store.remove(key).is_some() {
@@ -26,5 +27,40 @@ impl Database {
             }
         }
         removed
+    }
+
+    pub fn exists(&self, keys: &[String]) -> usize {
+        keys.iter()
+            .filter(|key| self.store.contains_key(*key))
+            .count()
+    }
+
+    pub fn incr(&mut self, key: &str) -> Result<i64, &'static str> {
+        let val = self.store.entry(key.to_string()).or_insert("0".to_string());
+        match val.parse::<i64>() {
+            Ok(num) => {
+                let new_num = num + 1;
+                *val = new_num.to_string();
+                Ok(new_num)
+            }
+            Err(_) => Err("Value is not a number"),
+        }
+    }
+    pub fn incr_by(&mut self, key: &str, by: i64) -> Result<i64, &'static str> {
+        let val = self.store.entry(key.to_string()).or_insert("0".to_string());
+        match val.parse::<i64>() {
+            Ok(num) => {
+                *val = (num + by).to_string();
+                Ok(num + by)
+            }
+            Err(_) => Err("value is not an integer"),
+        }
+    }
+    pub fn keys(&self) -> Vec<String> {
+        self.store.keys().cloned().collect()
+    }
+
+    pub fn flushdb(&mut self) {
+        self.store.clear();
     }
 }
--- src/resp.rs
+++ src/resp.rs
@@ -1,9 +1,9 @@
 use crate::db::Database;
 
-pub fn parse_resp(input : &str)-> Vec<String> {
+pub fn parse_resp(input: &str) -> Vec<String> {
     input
         .lines()
-        .filter(|line|!line.starts_with('*') && !line.starts_with('$'))
+        .filter(|line| !line.starts_with('*') && !line.starts_with('$'))
         .map(|line| line.to_string())
         .collect()
 }
@@ -13,21 +13,62 @@ pub fn handle_command(input: &str, db: &mut Database) -> String {
     match parts[0].to_uppercase().as_str() {
         "PING" => "+PONG\r\n".to_string(),
         "ECHO" => format!("${}\r\n{}\r\n", parts[1].len(), parts[1]),
-        "SET"  => {
+        "SET" => {
             db.set(parts[1].clone(), parts[2].clone());
             "+OK\r\n".to_string()
         }
-        "GET"  => {
-            if let Some(value) = db.get(&parts[1]){
+        "GET" => {
+            if let Some(value) = db.get(&parts[1]) {
                 format!("${}\r\n{}\r\n", value.len(), value)
             } else {
                 "$-1\r\n".to_string()
             }
         }
-        "DEL" =>{
+        "DEL" => {
             let deleted = db.delete(&parts[1..]);
             format!(":{}\r\n", deleted)
         }
-        _      => "-ERR unknown command\r\n".to_string(),
+        "EXISTS" => {
+            let exists = db.exists(&parts[1..]);
+            format!(":{}\r\n", exists)
+        }
+        "INCR" => match db.incr(&parts[1]) {
+            Ok(val) => format!(":{}\r\n", val),
+            Err(e) => format!("-ERR {}\r\n", e),
+        },
+        "INCRBY" => {
+            let by: i64 = parts[2].parse().unwrap_or(0);
+            match db.incr_by(&parts[1], by) {
+                Ok(val) => format!(":{}\r\n", val),
+                Err(e) => format!("-ERR {}\r\n", e),
+            }
+        }
+
+        "DECRBY" => {
+            let by: i64 = parts[2].parse().unwrap_or(0);
+            match db.incr_by(&parts[1], -by) {
+                Ok(val) => format!(":{}\r\n", val),
+                Err(e) => format!("-ERR {}\r\n", e),
+            }
+        }
+        "DECR" => match db.incr_by(&parts[1], -1) {
+            Ok(val) => format!(":{}\r\n", val),
+            Err(e) => format!("-ERR {}\r\n", e),
+        },
+        "KEYS" => {
+            let keys = db.keys();
+            let mut response = format!("*{}\r\n", keys.len());
+            for key in keys {
+                response.push_str(&format!("${}\r\n{}\r\n", key.len(), key));
+            }
+            response
+        }
+
+        "FLUSHDB" => {
+            db.flushdb();
+            "+OK\r\n".to_string()
+        }
+
+        _ => "-ERR unknown command\r\n".to_string(),
     }
 }
